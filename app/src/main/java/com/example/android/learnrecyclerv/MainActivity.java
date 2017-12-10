ab203933a6a365af0b2e7bd619ebd5aad6edb94e
Bayu Setiawan <bayusetiawan37@gmail.com>
2017-12-10 08:17:28 +0700

    getting data from internet

--- app/src/main/java/com/example/android/learnrecyclerv/MainActivity.java
+++ app/src/main/java/com/example/android/learnrecyclerv/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.android.learnrecyclerv;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<ListItem> listItems;
+    private static final String URL_DATA="https://api.myjson.com/bins/17hnxz";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +36,49 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        listItems=new ArrayList<>();
+
         makeData();
 
-        adapter=new MyAdapter(listItems,this);
-        recyclerView.setAdapter(adapter);
+        //adapter=new MyAdapter(listItems,this);
+        //recyclerView.setAdapter(adapter);
     }
 
     private void makeData() {
-        listItems=new ArrayList<>();
-        for (int i=0;i<=10;i++){
-            ListItem item=new ListItem(
-              "Heading "+i,
-                    "Lorem Ipsum test test"
-            );
+        final ProgressDialog progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.show();
 
-            listItems.add(item);
-        }
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, URL_DATA, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                try {
+                    JSONObject jsonObject=new JSONObject(response);
+                    JSONArray array=jsonObject.getJSONArray("post");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject o=array.getJSONObject(i);
+                        ListItem li=new ListItem(
+                                o.getString("title"),
+                                o.getString("desc")
+                        );
+                        listItems.add(li);
+                    }
+                    adapter=new MyAdapter(listItems,getApplicationContext());
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                error.printStackTrace();
+            }
+        });
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
